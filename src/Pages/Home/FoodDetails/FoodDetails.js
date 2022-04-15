cdfc03ee079a5d1b673bc469d5fd48c6f6346b20
forhad-eng <forhadu.cse@gmail.com>
2022-04-15 21:57:48 +0600

    routing added in selected product component

--- src/Pages/Home/FoodDetails/FoodDetails.js
+++ src/Pages/Home/FoodDetails/FoodDetails.js
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { NavLink, useParams } from 'react-router-dom'
 import { CartContext } from '../../../App'
 import useFood from '../../../hooks/useFood'
 import { addToCart } from '../../../utilities/fakedb'
@@ -32,35 +32,58 @@ const FoodDetails = () => {
             {foods.length === 0 ? (
                 <LoadingSpinner />
             ) : (
-                <div className="grid md:grid-cols-2 items-center px-10 md:px-24">
-                    <div className="leading-10 mx-auto">
-                        <p className="text-3xl font-semibold tracking-wide">{selected?.name}</p>
-                        <p>{selected?.description}</p>
-                        <div className="flex items-center gap-5 mb-3">
-                            <p className="text-4xl inline-block">${selected?.price * quantity}</p>{' '}
-                            <span className="shadow-lg rounded-full border-[1px] px-6">
-                                <button
-                                    className="outline-none mr-2"
-                                    onClick={() => {
-                                        quantity > 1 && setQuantity(quantity - 1)
-                                    }}
-                                >
-                                    -
-                                </button>
-                                {quantity}
-                                <button className="outline-none ml-2" onClick={() => setQuantity(quantity + 1)}>
-                                    +
-                                </button>
-                            </span>
-                        </div>
-                        <button
-                            onClick={addToCartHandler}
-                            className="text-sm text-white outline-none bg-red-500 px-5 py-2 block rounded-full mb-5"
+                <div>
+                    <div className="flex justify-center gap-6 my-6 font-bold">
+                        <NavLink
+                            to="/breakfast"
+                            className={({ isActive }) => (isActive ? 'text-red-500 border-b-2 border-red-700' : '')}
                         >
-                            🛒<span className="ml-2">Add</span>
-                        </button>
+                            Breakfast
+                        </NavLink>
+                        <NavLink
+                            to="/lunch"
+                            className={({ isActive }) => (isActive ? 'text-red-500 border-b-2 border-red-700' : '')}
+                        >
+                            Lunch
+                        </NavLink>
+                        <NavLink
+                            to="/dinner"
+                            className={({ isActive }) => (isActive ? 'text-red-500 border-b-2 border-red-700' : '')}
+                        >
+                            Dinner
+                        </NavLink>
                     </div>
-                    <img className="w-9/12 mx-auto" src={selected?.img} alt="" />
+
+                    <div className="grid md:grid-cols-2 items-center px-10 pb-20 md:px-24 ">
+                        <div className="leading-10 mx-auto">
+                            <p className="text-3xl font-semibold tracking-wide">{selected?.name}</p>
+                            <p>{selected?.description}</p>
+                            <div className="flex items-center gap-5 mb-3">
+                                <p className="text-4xl inline-block">${selected?.price * quantity}</p>{' '}
+                                <span className="shadow-lg rounded-full border-[1px] px-6">
+                                    <button
+                                        className="outline-none mr-2"
+                                        onClick={() => {
+                                            quantity > 1 && setQuantity(quantity - 1)
+                                        }}
+                                    >
+                                        -
+                                    </button>
+                                    {quantity}
+                                    <button className="outline-none ml-2" onClick={() => setQuantity(quantity + 1)}>
+                                        +
+                                    </button>
+                                </span>
+                            </div>
+                            <button
+                                onClick={addToCartHandler}
+                                className="text-sm text-white outline-none bg-red-500 px-5 py-2 block rounded-full mb-5"
+                            >
+                                🛒<span className="ml-2">Add</span>
+                            </button>
+                        </div>
+                        <img className="w-9/12 mx-auto" src={selected?.img} alt="" />
+                    </div>
                 </div>
             )}
         </>
