d311d9cc3e67c72880881a9cb8c331c802c9539f
forhad-eng <forhadu.cse@gmail.com>
2022-04-16 02:41:38 +0600

    home section input positioning error corrected

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -10,11 +10,11 @@ const Home = () => {
                 <p className="text-2xl md:text-5xl text-center pt-28 md:pt-36 mb-5">Best food is awaiting you</p>
                 <div className="w-3/4 md:w-1/2 mx-auto flex justify-center items-center relative">
                     <input
-                        className="rounded-full outline-none py-1 pl-4 mr-12 md:mr-16 md:py-2 md:pl-5 md:pr-32"
+                        className="rounded-full outline-none py-1 px-8 md:mr-16 md:py-2 md:pl-5 md:pr-32"
                         type="text"
                         placeholder="Search food items"
                     />
-                    <button className="text-sm text-white outline-none bg-red-500 px-5 py-1 md:py-2 rounded-full absolute right-2 md:right-[40px] lg:right-[90px] xl:right-[140px]">
+                    <button className="text-sm text-white outline-none bg-red-500 px-5 py-1 md:py-2 rounded-full absolute right-0 md:right-[40px] lg:right-[90px] xl:right-[140px]">
                         Search
                     </button>
                 </div>
